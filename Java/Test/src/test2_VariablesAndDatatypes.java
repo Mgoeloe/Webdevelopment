76189f3c0828511119c429e39124700dd6691e69
Marlon <goeloe@gmail.com>
2021-01-30 17:54:15 +0100

    Added Java Tryout

---
+++ Java/Test/src/test2_VariablesAndDatatypes.java
@@ -0,0 +1,17 @@
+public class test2_VariablesAndDatatypes {
+    public static void main(String[] args){
+        char var1 = 'h';
+        String var2 = "Hi, my name is Marlon";
+        boolean var3 = true;
+        int var4 = 91;
+        double var5 = 3.141592653;
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+
+
+
+    }
+}
---
+++ Java/Test/src/test3_BasicMathOperations.java
@@ -0,0 +1,16 @@
+public class test3_BasicMathOperations {
+    public static void main(String[] args){
+        double var1 = 5;
+        double var2 = 7;
+        double var3 = 10;
+        double var5 = var1 + var2;
+        double var4 = 20;
+        String var6 = "Hello, I am ";
+        String var7 = "years old";
+
+        System.out.println(var6);
+        System.out.println(var1 + var3 * var4 / var5);
+        System.out.println(var7);
+
+    }
+}
