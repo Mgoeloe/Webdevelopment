f74bfb76c5349fa749c7ef9b358dad455bd798eb
Marlon <goeloe@gmail.com>
2021-02-17 16:57:10 +0100

    Updated Java Opdracht 2

--- Java/Opdrachten week 2/src/Bear.java
+++ Java/Opdrachten week 2/src/Bear.java
@@ -1,5 +1,5 @@
 public class Bear extends Animal {
-    private String type;
+    String type;
 
     public Bear(String naam, int leeftijd, String type){
         super(naam, leeftijd);
@@ -7,7 +7,7 @@ public class Bear extends Animal {
         this.type = type;
     }
 
-    public static void showType(Bear bear1, Bear bear2) {
+    public void showType() {
         System.out.println(type);
 //        System.out.println(bear2.type);
         System.out.println(Animal.type);
@@ -17,8 +17,19 @@ public class Bear extends Animal {
     public static void main(String[] args) {
 
         Bear bear2 = new Bear("Henk", 17, "Polar Bear");
-        Bear bear1 = new Bear("piet", 43, "Brown Bear");
-        showType(bear1, bear2);
+        Bear bear1 = new Bear("Piet", 43, "Brown Bear");
+        Animal pinguïn = new Animal("Bob", 18);
+//        bear1.showType();
+//        bear2.showType();
+
+        Bear bears[] = new Bear[2];
+        bears[0] = bear1;
+        bears[1] = bear2;
+        Animal animals[] = {bear1, bear2, pinguïn};
+
+
+
+        System.out.println(animals[2].naam);
 
     }
 
