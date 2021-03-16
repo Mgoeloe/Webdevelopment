0bf9811031d0988394f7d8bbc57c8d9c03d80af3
Marlon <goeloe@gmail.com>
2021-03-16 17:00:12 +0100

    Updated WeatherApp

--- React/WeatherApp/my-app/src/CardDetail.js
+++ React/WeatherApp/my-app/src/CardDetail.js
@@ -1,44 +1,61 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import uuid from 'uuidv4';
 
 
-const CardDetail = ({lat, lon}) => {
+const CardDetail = ({ lat, lon }) => {
 
-    const apiKey = '77bc13a3ad3deb011bf46df0dd99ca9b';
-    const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly&appid=${apiKey}`;
-  
-    const [weatherDetail, setWeatherDetail] = useState(null);
-  
+  const apiKey = '77bc13a3ad3deb011bf46df0dd99ca9b';
+  const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly&appid=${apiKey}`;
 
-    useEffect(() => {
-      fetchData();
-    }, []);
+  const [weatherDetail, setWeatherDetail] = useState();
+  const [date, setDate] = useState();
 
-    const fetchData = async () => {
-      const response = await axios.get(apiURL);
-  
-      setWeatherDetail(response.data);
-      console.log(response);}
+  const { uuid } = require('uuidv4');
 
-    // console.log(weatherDetail);
-    
-    // const d = new Date(weatherDetail.daily[0].dt*1000);
-    // const n = d.toLocaleDateString();
-    // console.log(n);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
 
+  const fetchData = async () => {
+    const response = await axios.get(apiURL);
 
-    return (
-        <div className='weathercard'>        
-          {weatherDetail && (
-            <div>
-              <p>Datum: {weatherDetail.current.dt}</p>
-              
-            </div>
-          )}
-            
+    setWeatherDetail(response.data);
+    setDate(response.data.daily);
+    // console.log(response);
+  }
+
+  console.log(weatherDetail);
+
+  const d = new Date(date*1000);
+  const n = d.toLocaleDateString();
+  // console.log(n);
+
+  // console.log(date);
+
+  return (
+    <div className='weathercard'>
+      {/* {weatherDetail && (
+        <div>
+          <p>Datum: {n}</p>
+
         </div>
-    )
+      )} */}
+
+      {date && 
+        date.map((datum, index) => {
+
+        return (
+        <div key='{uuid}'>
+          <p>Datum: {datum.index}</p>
+          {console.log(index)}
+        </div>
+        );
+      })}
+
+    </div>
+  )
 }
 
 export default CardDetail
